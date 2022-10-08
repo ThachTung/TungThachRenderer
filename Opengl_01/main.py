df757352870cb1318bac8d86fd3fb316875eb9ea
Tung Thach <tungthach1705@gmail.com>
2022-10-08 21:20:22 +0700

    +camera and texture mapping

--- Opengl_01/main.py
+++ Opengl_01/main.py
@@ -3,7 +3,7 @@ import moderngl as mgl
 import sys
 from model import *
 from camera import Camera
-
+from light import Light
 
 class GraphicsEngine:
     def __init__(self, win_size=(1280,720)):
@@ -17,11 +17,22 @@ class GraphicsEngine:
         pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
         #create opengl context
         pg.display.set_mode(self.WIN_SIZE, flags=pg.OPENGL | pg.DOUBLEBUF)
+        #mouse settings
+        pg.event.set_grab(True)
+        pg.mouse.set_visible(False)
+        
         #detect and use existing opengl context
         self.ctx = mgl.create_context()
+        #enable flag for seeing outside of cube
+        #self.ctx.front_face = 'cw' # backface culling
+        self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE)
         #create an object to help track time
         self.clock = pg.time.Clock()
         self.time = 0
+        self.delta_time = 0
+
+        #light
+        self.light = Light()
 
         #camera
         self.camera= Camera(self)
@@ -53,8 +64,9 @@ class GraphicsEngine:
         while True:
             self.get_time()
             self.check_events()
+            self.camera.update()
             self.render()
-            self.clock.tick(60)
+            self.delta_time = self.clock.tick(60)
 
 if __name__ == '__main__':
     app = GraphicsEngine()
