f2e6976927180693ff3d0a54952ba8af2671a7b4
Tung Thach <tungthach1705@gmail.com>
2022-10-13 22:19:27 +0700

    !BackupFolder

---
+++ Opengl_02/model.py
@@ -0,0 +1,62 @@
+import moderngl as mgl
+import numpy as np
+import glm
+
+
+class BaseModel:
+    def __init__(self, app, vao_name, tex_id, pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
+        self.app = app
+        self.pos = pos
+        self.vao_name = vao_name
+        self.rot = glm.vec3([glm.radians(a) for a in rot])
+        self.scale = scale
+        self.m_model = self.get_model_matrix()
+        self.tex_id = tex_id
+        self.vao = app.mesh.vao.vaos[vao_name]
+        self.program = self.vao.program
+        self.camera = self.app.camera
+
+    def update(self): ...
+
+    def get_model_matrix(self):
+        m_model = glm.mat4()
+        return m_model
+
+    def render(self):
+        self.update()
+        self.vao.render()
+
+
+class ExtendedBaseModel(BaseModel):
+    def __init__(self, app, vao_name, tex_id, pos, rot, scale):
+        super().__init__(app, vao_name, tex_id, pos, rot, scale)
+        self.on_init()
+
+    def update(self):
+        self.texture.use(location=0)
+        self.program['camPos'].write(self.camera.position)
+        self.program['m_view'].write(self.camera.m_view)
+        self.program['m_model'].write(self.m_model)
+
+    def on_init(self):
+        # texture
+        self.texture = self.app.mesh.texture.textures[self.tex_id]
+        self.program['u_texture'] = 0
+        self.program['n_texture'] = 1
+        self.texture.use(location=0)
+
+        # mvp
+        self.program['m_proj'].write(self.camera.m_proj)
+        self.program['m_view'].write(self.camera.m_view)
+        self.program['m_model'].write(self.m_model)
+        # light
+        self.program['light.position'].write(self.app.light.position)
+        self.program['light.Ia'].write(self.app.light.Ia)
+        self.program['light.Id'].write(self.app.light.Id)
+        self.program['light.Is'].write(self.app.light.Is)
+
+
+class Cat(ExtendedBaseModel):
+    def __init__(self, app, vao_name='cat', tex_id='cat',
+                 pos=(0, 0, 0), rot=(-90, 0, 0), scale=(1, 1, 1)):
+        super().__init__(app, vao_name, tex_id, pos, rot, scale)
---
+++ Opengl_02/vao.py
@@ -0,0 +1,23 @@
+from vbo import VBO
+from shader_program import ShaderProgram
+
+
+class VAO:
+    def __init__(self, ctx):
+        self.ctx = ctx
+        self.vbo = VBO(ctx)
+        self.program = ShaderProgram(ctx)
+        self.vaos = {}
+
+        # cat vao
+        self.vaos['cat'] = self.get_vao(
+            program=self.program.programs['default'],
+            vbo=self.vbo.vbos['cat'])
+
+    def get_vao(self, program, vbo):
+        vao = self.ctx.vertex_array(program, [(vbo.vbo, vbo.format, *vbo.attribs)], skip_errors=True)
+        return vao
+
+    def destroy(self):
+        self.vbo.destroy()
+        self.program.destroy()
