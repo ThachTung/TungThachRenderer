07b4aace31005aea4a95dc1d5aa1555d6796adea
Tung Thach <tungthach1705@gmail.com>
2022-11-23 01:39:32 -0800

    add slider to control lighting intensity

--- Opengl_01/light.py
+++ Opengl_01/light.py
@@ -1,6 +1,6 @@
 import glm
 import pygame
-
+from tkinter import  *
 class Light:
     def __init__(self, position=(3.0,3.0,3.0), color=(1.0,1.0,1.0)):
         self.position = glm.vec3(position)
@@ -9,12 +9,18 @@ class Light:
         self.ambient_intensity = self.color * 0.5
         self.diffuse_intensity = self.color * self.diffuse_value
         self.specular_intensity = self.color * 0.0
+
+        self.diffuse_light_label = Label(text="Diffuse Light Value")
+        self.diffuse_light_slider = Scale(from_=0,to=100,command=self.get_value,orient=HORIZONTAL)
+        self.pack_function()
+
+    def pack_function(self):
+        self.diffuse_light_label.pack()
+        self.diffuse_light_slider.pack()
+    def get_value(self,value):
+        self.diffuse_value = float(value)
+        self.diffuse_intensity = self.color * self.diffuse_value
     def light_change(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            self.diffuse_value = self.diffuse_value + 10.0
-            self.diffuse_intensity = self.color * self.diffuse_value
-        if keys[pygame.K_s]:
-            self.diffuse_value = self.diffuse_value - 10.0
-            self.diffuse_intensity = self.color * self.diffuse_value
+        self.diffuse_light_slider.update()
+
 
--- Opengl_01/main.py
+++ Opengl_01/main.py
@@ -16,8 +16,8 @@ class GraphicsEngine:
         pygame.display.gl_set_attribute(pygame.GL_CONTEXT_PROFILE_MASK, pygame.GL_CONTEXT_PROFILE_CORE)
 
         pygame.display.set_mode(self.WIN_SIZE, flags=pygame.OPENGL | pygame.DOUBLEBUF)
-        pygame.event.set_grab(True)
-        pygame.mouse.set_visible(False)
+        #pygame.event.set_grab(True)
+        #pygame.mouse.set_visible(False)
 
         self.ctx = moderngl.create_context()
         #enable flag for seeing outside of cube
@@ -29,8 +29,9 @@ class GraphicsEngine:
         self.delta_time = 0
 
         self.light = Light()
-        self.camera= Camera(self)
+        self.camera = Camera(self)
         self.scene = Model(self)
+
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
@@ -50,8 +51,9 @@ class GraphicsEngine:
         while True:
             self.get_time()
             self.check_events()
-            self.camera.update()
             self.light.light_change()
+            #print(self.light.position)
+            self.camera.update()
             self.render()
             self.delta_time = self.clock.tick(60)
 
